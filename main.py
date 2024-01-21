0397b2ae8de1665071e796778641c00cbd084ecc
Cager <filip.crnogorac@fpmoz.sum.ba>
2024-01-21 17:20:06 +0100

    Update

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ app.add_middleware(
 def webScrape(url):
     chrome_options = FirefoxOptions()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--remote-debugging-port=9222')
     chrome_options.add_argument('--blink-settings=imagesEnabled=false')
 
     with webdriver.Edge(options=chrome_options) as driver:
