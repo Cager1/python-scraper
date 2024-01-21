a5d207dcd208918199ddba4faa7c0601b31843e9
Cager <filip.crnogorac@fpmoz.sum.ba>
2024-01-21 12:32:08 +0100

    Update

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ app.add_middleware(
 def webScrape(url):
     chrome_options = ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--user-data-dir=/home/cager')
